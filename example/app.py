9e5a9c7630b292b536b993bc655ea0973ce63c9e
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2023-11-09 10:01:04 +0000

    add powerflow router
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- example/app.py
+++ example/app.py
@@ -2,5 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title=" API") 
 
-from power_grid.router import router
-app.include_router(router, prefix="/api")
+from power_grid.router import router as pg_router
+from pgm_powerflow.router import router as pf_router
+app.include_router(pg_router, prefix="/api")
+app.include_router(pf_router, prefix="/api")
--- example/pgm_powerflow/router.py
+++ example/pgm_powerflow/router.py
@@ -1,13 +1,10 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from payment_calculation.aux_models import JobComplete
-from payment_calculation.models import EmployeeIn, EmployeeOut
+from pgm_powerflow.aux_models import JobComplete
+from pgm_powerflow.models import PGM_Powerflow
 
-router = APIRouter(prefix="/payment_calculation")
+router = APIRouter(prefix="/pgm_powerflow")
 
-
-class TYPE_UPDATE(BaseModel):
-    pass # TODO replace with a real example
 
 @router.get("/")
 async def get_all_payment_calculation() -> list[str]:
@@ -17,11 +14,11 @@ async def get_all_payment_calculation() -> list[str]:
 
 @router.post("/")
 async def new_payment_calculation(
-    resource: EmployeeIn,
+    resource: PGM_Powerflow,
 ) -> JobComplete:  # TODO should be wrapped in jonb
     # raise NotImplementedError()  # TODO This should create a new job entry in DB
-    emp = EmployeeOut(**resource.model_dump(), personal_nr="1")
-    job = JobComplete(id="test",input=emp)
+    pf = PGM_Powerflow(**resource.model_dump())
+    job = JobComplete(id="test", input=pf)
     return job
 
 
@@ -30,10 +27,10 @@ async def get_payment_calculation(id: str) -> JobComplete:
     raise NotImplementedError()  # TODO fetch Job with ID from the DB
 
 
-@router.put("/{id}")
-async def update_payment_calculation(resource: TYPE_UPDATE) -> JobComplete:
-    raise NotImplementedError()  # TODO update the job in the db, this might be impossible in our structure since worker might have a differnt verison of this
-    # Maybe check status is pending, success or failed to restart
+# @router.put("/{id}")
+# async def update_payment_calculation(resource: TYPE_UPDATE) -> JobComplete:
+#     raise NotImplementedError()  # TODO update the job in the db, this might be impossible in our structure since worker might have a differnt verison of this
+#     # Maybe check status is pending, success or failed to restart
 
 
 @router.delete("/{id}")
