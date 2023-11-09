5bfbbc1438720ba138d450a0042588fa101ffba0
Martijn Govers <Martijn.Govers@Alliander.com>
2023-11-09 16:19:19 +0100

    remove deprecated
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- src/pgm_service/power_grid/power_grid_model.py
+++ src/pgm_service/power_grid/power_grid_model.py
@@ -12,37 +12,6 @@ from pgm_service.power_grid.models import Grid
 from pgm_service.power_grid.cgmes_pgm_converter import System as CGMESToPGMConverter
 
 
-def download_data(url):
-    def download_grid_data(name, url):
-        with open(name, "wb") as out_file:
-            content = requests.get(url, stream=True).content
-            out_file.write(content)
-
-    url = "https://raw.githubusercontent.com/dpsim-simulator/cim-grid-data/master/BasicGrids/NEPLAN/Slack_Load_Line_Sample/"
-    filename = "Rootnet_FULL_NE_19J18h"
-
-    download_grid_data(filename + "_EQ.xml", url + filename + "_EQ.xml")
-    download_grid_data(filename + "_TP.xml", url + filename + "_TP.xml")
-    download_grid_data(filename + "_SV.xml", url + filename + "_SV.xml")
-
-    files = glob.glob(filename + "_*.xml")
-
-    print("CGMES files downloaded:")
-    print(files)
-
-    this_file_folder = Path(__file__).parents[3]
-    p = str(this_file_folder)
-    xml_path = Path(p)
-    xml_files = [
-        os.path.join(xml_path, filename + "_EQ.xml"),
-        os.path.join(xml_path, filename + "_TP.xml"),
-        os.path.join(xml_path, filename + "_SV.xml"),
-    ]
-
-    print(xml_files)
-    return xml_files
-
-
 def create_model(grid: Grid):  # TODO make async
     # xml_files = download_data(url=grid.input_data)
     with tempfile.TemporaryDirectory(dir="./cim_files") as tmpdir:
