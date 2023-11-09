50e5c6040dd37a3ffd939519689d4cc1e1e5f2dc
Tony Xiang <tony.xiang@alliander.com>
2023-11-09 10:24:05 +0100

    can run from python
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- src/pgm_service/main.py
+++ src/pgm_service/main.py
@@ -1,24 +1,6 @@
-from power_grid_model import initialize_array
-
-from fastapi import FastAPI
-
 import uvicorn
-
-app = FastAPI()
-
-
-@app.get("/initialize/{dataset_type}/{component}/{size}")
-def initialize(dataset_type: str, component: str, size: int):
-    arr = initialize_array(dataset_type, component, size)
-    return repr(arr)
-
-
-@app.get("/")
-def initialize_default():
-    arr = initialize_array("input", "node", 1)
-    return repr(arr)
 
 
 def run():
-    # uvicorn.run("main:app", port=5000, log_level="info")
-    print("Hello world!")
+    print("Starting unvicorn service.")
+    uvicorn.run("pgm_service.app:app", port=5000, log_level="info")
