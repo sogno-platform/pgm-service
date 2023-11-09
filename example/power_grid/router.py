03c171cfaace2b7876703de312d3c6c056158f77
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2023-11-09 09:42:24 +0000

    add grid router
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- example/power_grid/router.py
+++ example/power_grid/router.py
@@ -1,13 +1,10 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from payment_calculation.aux_models import JobComplete
-from payment_calculation.models import EmployeeIn, EmployeeOut
+from power_grid.aux_models import JobComplete
+from power_grid.models import Grid
 
-router = APIRouter(prefix="/payment_calculation")
+router = APIRouter(prefix="/power_grid")
 
-
-class TYPE_UPDATE(BaseModel):
-    pass # TODO replace with a real example
 
 @router.get("/")
 async def get_all_payment_calculation() -> list[str]:
@@ -17,11 +14,11 @@ async def get_all_payment_calculation() -> list[str]:
 
 @router.post("/")
 async def new_payment_calculation(
-    resource: EmployeeIn,
+    resource: Grid,
 ) -> JobComplete:  # TODO should be wrapped in jonb
     # raise NotImplementedError()  # TODO This should create a new job entry in DB
-    emp = EmployeeOut(**resource.model_dump(), personal_nr="1")
-    job = JobComplete(id="test",input=emp)
+    resource = Grid(**resource.model_dump(), personal_nr="1")
+    job = JobComplete(id="test",input=resource)
     return job
 
 
@@ -29,11 +26,6 @@ async def new_payment_calculation(
 async def get_payment_calculation(id: str) -> JobComplete:
     raise NotImplementedError()  # TODO fetch Job with ID from the DB
 
-
-@router.put("/{id}")
-async def update_payment_calculation(resource: TYPE_UPDATE) -> JobComplete:
-    raise NotImplementedError()  # TODO update the job in the db, this might be impossible in our structure since worker might have a differnt verison of this
-    # Maybe check status is pending, success or failed to restart
 
 
 @router.delete("/{id}")
