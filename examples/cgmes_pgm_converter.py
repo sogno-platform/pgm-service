5f6c69d12eb6e4d2831fd01065f475a45616cea9
Peter Salemink <peter.salemink@alliander.com>
2023-11-09 12:07:33 +0100

    temp
    Signed-off-by: Peter Salemink <peter.salemink@alliander.com>

--- examples/cgmes_pgm_converter.py
+++ examples/cgmes_pgm_converter.py
@@ -23,7 +23,7 @@ class System():
         # list_SvPowerFlow = [elem for elem in res.values() if elem.__class__.__name__ == "SvPowerFlow"]
         # list_EnergySources = [elem for elem in res.values() if elem.__class__.__name__ == "EnergySource"]
         # list_EnergyConsumer = [elem for elem in res.values() if elem.__class__.__name__ == "EnergyConsumer"]
-        # list_ACLineSegment = [elem for elem in res.values() if elem.__class__.__name__ == "ACLineSegment"]
+        list_ACLineSegment = [elem for elem in res.values() if elem.__class__.__name__ == "ACLineSegment"]
         # list_Terminals = [elem for elem in res.values() if elem.__class__.__name__ == "Terminal"]
         # list_Terminals_ES = [elem for elem in list_Terminals if
         #                      elem.ConductingEquipment.__class__.__name__ == "EnergySource"]
@@ -35,52 +35,59 @@ class System():
             self.nodes.append(TPNode.mRID)
             self.voltages.append(dict_VoltageLevel[TPNode])
 
-
-
-        #
-        #
-        # self._setNodeType(list_Terminals)
-        #
         # # create branches type ACLineSegment
         # for ACLineSegment in list_ACLineSegment:
-        #     uuid_ACLineSegment = ACLineSegment.mRID
-        #     nodes = self._get_nodes(list_Terminals, uuid_ACLineSegment)
-        #     start_node = nodes[0]
-        #     end_node = nodes[1]
-        #
-        #     base_voltage = ACLineSegment.BaseVoltage.nominalVoltage
-        #     self.branches.append(Branch(uuid=uuid_ACLineSegment, r=ACLineSegment.r, x=ACLineSegment.x,
-        #                                 start_node=start_node, end_node=end_node,
-        #                                 base_voltage=base_voltage, base_apparent_power=base_apparent_power))
-        #
-        # # create branches type powerTransformer
-        # for power_transformer in list_PowerTransformer:
-        #     uuid_power_transformer = power_transformer.mRID
-        #     nodes = self._get_nodes(list_Terminals, uuid_power_transformer)
-        #     start_node = nodes[0]
-        #     end_node = nodes[1]
-        #
-        #     # base voltage = high voltage side (=primaryConnection)
-        #     primary_connection = self._get_primary_connection(list_PowerTransformerEnds, uuid_power_transformer)
-        #     base_voltage = primary_connection.BaseVoltage.nominalVoltage
-        #     self.branches.append(Branch(uuid=uuid_power_transformer, r=primary_connection.r, x=primary_connection.x,
-        #                                 start_node=start_node, end_node=end_node, base_voltage=base_voltage,
-        #                                 base_apparent_power=base_apparent_power))
-        #
-        # # create breakers
-        # for obj_Breaker in list_Breakers:
-        #     is_open = obj_Breaker.normalOpen
-        #     nodes = self._get_nodes(list_Terminals, obj_Breaker.mRID)
-        #     self.breakers.append(Breaker(from_node=nodes[0], to_node=nodes[1], is_open=is_open))
-        #
-        #     # if the breaker is open == closed --> close broker
-        #     if is_open is False:
-        #         self.breakers[-1].close_breaker()
-        #     else:
-        #         self.breakers[-1].ideal_connected_with = ''
-        #
-        # # calculate admitance matrix
-        # self.Ymatrix_calc()
+            # line = initialize_array("input", "line", len(self.lines))
+            #
+            # # Map each Line mRID to the assigned numerical Line ID
+            # line["id"] = mapd(self.lines.keys(), self.asset_lookup)
+            #
+            # # Map the terminal mRID on the from/to side of each line to a Node mRID,
+            # # and the Node mRID to the assigned numerical Node ID
+            # line["from_node"] = mapd(
+            #     mapd(
+            #         self.lines.values(),
+            #         self.terminals_to_node_lookup,
+            #         lambda l: l.terminal_from,
+            #         "Line-from Terminal",
+            #         "Topology",
+            #     ),
+            #     self.asset_lookup,
+            # )
+            # line["to_node"] = mapd(
+            #     mapd(
+            #         self.lines.values(),
+            #         self.terminals_to_node_lookup,
+            #         lambda l: l.terminal_to,
+            #         "Line-to Terminal",
+            #         "Topology",
+            #     ),
+            #     self.asset_lookup,
+            # )
+            # # Map the terminal mRID on the from/to side of each line to a terminal status
+            # line["from_status"] = mapd(
+            #     self.lines.values(),
+            #     self.terminal_status,
+            #     lambda l: l.terminal_from,
+            #     "Line-from Status",
+            #     "SteadyStateHypothesis",
+            # )
+            # line["to_status"] = mapd(
+            #     self.lines.values(),
+            #     self.terminal_status,
+            #     lambda l: l.terminal_to,
+            #     "Line-to Status",
+            #     "SteadyStateHypothesis",
+            # )
+            #
+            # # Extract the Line parameters
+            # # TODO r0, x0, c0 and tan0 are not defined yet, since they are only needed for asymmetrical calculations
+            # line["r1"] = [single_line.r for single_line in self.lines.values()]
+            # line["x1"] = [single_line.x for single_line in self.lines.values()]
+            # line["c1"] = [single_line.bch / (2 * np.pi * FREQUENCY) for single_line in self.lines.values()]
+            # line["tan1"] = [single_line.gch / single_line.bch for single_line in self.lines.values()]
+            # line["i_n"] = [single_line.ratedCurrent for single_line in self.lines.values()]
+
 
 
     def create_pgm_input(self):
